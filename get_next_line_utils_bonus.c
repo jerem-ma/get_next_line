bd7743301e658d93f902a84cbb9915410c6d2c94
Jeremy Maia <jmaia@student.42.fr>
2021-11-30 12:26:35 +0100

    Oops double free

--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/25 12:54:08 by jmaia             #+#    #+#             */
-/*   Updated: 2021/11/30 12:22:59 by jmaia            ###   ########.fr       */
+/*   Updated: 2021/11/30 12:25:34 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ t_char_file	*get_next_char(t_file	*file)
 	if (file->is_end)
 	{
 		free(file->buffer);
+		file->buffer = 0;
 		c_file->is_end = 1;
 		return (c_file);
 	}
